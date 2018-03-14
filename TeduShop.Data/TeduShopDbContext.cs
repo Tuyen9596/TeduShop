b6ae51c6f8d4815a2dd22d3c8b842d1d21abde16
= <tuxenpham@gmail.com>
2018-03-14 09:58:03 +0700

    fix loi

--- TeduShop.Data/TeduShopDbContext.cs
+++ TeduShop.Data/TeduShopDbContext.cs
@@ -37,8 +37,6 @@ namespace TeduShop.Data
 
         public DbSet<VisitorStatistic> VisitorStatistics { set; get; }
         public DbSet<Error> Errors { set; get; }
-        public DbSet<ContactDetail> ContactDetails { set; get; }
-        public DbSet<Feedback> Feedbacks { set; get; }
 
 
         public static TeduShopDbContext Create()
