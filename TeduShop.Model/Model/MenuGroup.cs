dcbafd2f1ba466cabbd7b65c17771f138cc89155
= <tuxenpham@gmail.com>
2018-03-20 22:29:37 +0700

    fix mapping

--- TeduShop.Model/Model/MenuGroup.cs
+++ TeduShop.Model/Model/MenuGroup.cs
@@ -1,20 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace TeduShop.Model
+namespace TeduShop.Model.Model
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-
-    [Table("MenuGroup")]
-    public partial class MenuGroup
+    [Table("MenuGroups")]
+    public class MenuGroup
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ID { get; set; }
+        public int ID { set; get; }
+
         [Required]
-        public string Name { get; set; }
-    
-        public virtual IEnumerable<Menu> Menus { get; set; }
+        [MaxLength(50)]
+        public string Name { set; get; }
+
+        public virtual IEnumerable<Menu> Menus { set; get; }
     }
 }
