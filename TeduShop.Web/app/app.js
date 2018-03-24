4bdf5fc44a14292f28d7107c2776c4576a0e94f2
= <tuxenpham@gmail.com>
2018-03-24 09:10:30 +0700

    hh

--- TeduShop.Web/app/app.js
+++ TeduShop.Web/app/app.js
@@ -1,1 +1,12 @@
-﻿
+﻿/// <reference path="E:\GitHub\Project Visual 2012\C #\ASP.NET\TeduShop\TeduShop.Web\Content/admin/libs/angular/angular.js" />
+(function () {
+    angular.module('tedushop', []).config(config);
+    config.$injector = ['$stateProvider', '$urlRouterProvider'];
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/homeView.html",
+            controller: "homeCotroller"
+        });
+    }
+});
---
+++ TeduShop.Web/app/shared/modules/tedushop.common.js
@@ -0,0 +1,4 @@
+﻿/// <reference path="E:\GitHub\Project Visual 2012\C #\ASP.NET\TeduShop\TeduShop.Web\Content/admin/libs/angular/angular.js" />
+(function () {
+    angular.module('tedushop.common',[''])
+})
